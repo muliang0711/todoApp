a99636b2a4a3cb0dec73bdf45b1d490faf92ccc0
muliang <hockyangpui@gmail.com>
2025-05-11 19:19:22 +0800

    done Routes level and test start

--- backend/src/routes/TaskRoutes.ts
+++ backend/src/routes/TaskRoutes.ts
@@ -4,8 +4,22 @@ import { sessionCheckMiddleware } from "../middlewares/sessionCheckMiddleware";
 
 // 1. Create a router instance
 const router = Router();
-// 2. Define routes for task-related operations
-// 3. POST /api/tasks/create - Create a new task
+// 2. Define routes for task-related operations :
+
+// 1. POST /api/tasks/create - Create a new task
 router.post("/create", sessionCheckMiddleware, TaskController.createTask);
-router.post{"/delete", sessionCheckMiddleware, TaskController.deleteTask};
+// 2. DELETE /api/tasks/delete/:taskId - Delete a task
+router.delete("/delete/:taskId", sessionCheckMiddleware, TaskController.deleteTask);
+// 3. POST /api/tasks/update - Update an existing task
+router.post("/update", sessionCheckMiddleware, TaskController.updateTask);
+// 4. GET /api/tasks/getAll - Get all tasks
+router.get("/getAll", sessionCheckMiddleware, TaskController.getAllTasksByUserId);
+// 5. GET /api/tasks/getTaksInProgress - Get tasks in progress
+router.get("/getInProgress", sessionCheckMiddleware, TaskController.getTaskInProgress);
+// 6. GET /api/tasks/getCompleted - Get completed tasks
+router.get("/getCompleted", sessionCheckMiddleware, TaskController.getTaskInCompleted);
+// 7. GEt /api/taks/getBYDate/:date - Get tasks by date
+router.get("/getByDate/:date", sessionCheckMiddleware, TaskController.getTaskByDate);
+
+
 export default router;
